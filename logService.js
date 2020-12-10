46151d3aa6524575fcb0ec9e79ef2ad693277da7
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-12-10 02:10:23 -0500

    Made file name to be for each Day

--- logService.js
+++ logService.js
@@ -1,22 +1,24 @@
 const fs = require('fs');
-
+const fileName = () => {
+    let d = new Date(Date.now()).toISOString()
+    return d.split("T")[0];
+}
 const logData = (_obj) => {
 
     try {
         let jsonArray;
 
         //Check the 
-        if (fs.existsSync('.\\prices\\prices.json')) {
-            console.log("in here")
-            fs.readFile('.\\prices\\prices.json', (err, data) => {
+        if (fs.existsSync(`.\\prices\\${fileName()}.json`)) {
+            fs.readFile(`.\\prices\\${fileName()}.json`, (err, data) => {
                 jsonArray = JSON.parse(data);
                 jsonArray.push(_obj);
-                fs.writeFile(`.\\prices\\prices.json`, JSON.stringify(jsonArray), function (err) {
+                fs.writeFile(`.\\prices\\${fileName()}.json`, JSON.stringify(jsonArray), function (err) {
                     if (err) throw err;
                 });
             });
         } else {
-            fs.writeFile(".\\prices\\prices.json", "[]", () => console.log("Making new File"))
+            fs.writeFile(`.\\prices\\${fileName()}.json`, "[]", () => console.log("Making new File"))
             jsonArray = [];
         }
     } catch (err) {
